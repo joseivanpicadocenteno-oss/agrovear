8083e31d40ebcdf6200bfc53bb26d2f2f0b6b150
JoseIvanPicado <Jose.Picado8@tecnacional.edu.ni>
2026-07-28 10:20:08 -0600

    Fix: Correcion de error de sintaxis en Modelo de Recipe con sus useApp/Recipedetail_FeedingRecord

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use App\Models\RecipeDetails;
-use App\Models\FeedingRecords;
+use App\Models\RecipeDetail;
+use App\Models\FeedingRecord;
 use App\Models\Farm;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
