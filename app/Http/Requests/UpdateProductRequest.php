608096f1c5819d6672586c30382dc4cd2350954b
JoseIvanPicado <Jose.Picado8@tecnacional.edu.ni>
2026-07-28 09:31:42 -0600

    Fix: Correcion en uso de autorizacion; return true en UpdateProductRequest

--- app/Http/Requests/UpdateProductRequest.php
+++ app/Http/Requests/UpdateProductRequest.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateProductRequest extends FormRequest
@@ -12,13 +11,13 @@ class UpdateProductRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, ValidationRule|array<mixed>|string>
+     * // @return array<string, ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
