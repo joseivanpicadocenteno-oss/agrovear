3f8dad9c6e199ece3cd0a43542ba61aa3eb3f151
JoseIvanPicado <Jose.Picado8@tecnacional.edu.ni>
2026-07-28 10:29:27 -0600

    Fix(UpdateRecipeRequest): Agrgando reglas y corrigiendo metodos use

--- app/Http/Requests/UpdateRecipeRequest.php
+++ app/Http/Requests/UpdateRecipeRequest.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateRecipeRequest extends FormRequest
@@ -12,18 +11,29 @@ class UpdateRecipeRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, ValidationRule|array<mixed>|string>
+     * // @return array<string, ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
         return [
-            //
+        'name' => 'sometimes|string|max:255',
+        'description' => 'sometimes|string|max:255',
+        'objective' => 'sometimes|string|max:255',
+        'frequent_use' => 'sometimes|string|max:255',
+        'filter_species' => 'sometimes|string|max:255',
+        'min_age_filter' => 'sometimes|integer',
+        'max_age_filter' => 'sometimes|integer',
+        'min_weight_filter' => 'sometimes|numeric',
+        'recommended_duration_days' => 'sometimes|integer',
+        'suitable_for_gestation' => 'sometimes|boolean',
+        'suitable_for_location' => 'sometimes|boolean',
+        'farm_id' => 'sometimes|exists:farms,id',
         ];
     }
 }
