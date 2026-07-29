e3861e1b2d1350effd4c330b32e16d8bf6c285d1
JoseIvanPicado <Jose.Picado8@tecnacional.edu.ni>
2026-07-29 13:34:19 -0600

    Fix(UpdateTreatmentDetail): Correcion de metodo use

--- app/Http/Requests/UpdateTreatmentDetailRequest.php
+++ app/Http/Requests/UpdateTreatmentDetailRequest.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateTreatmentDetailRequest extends FormRequest
@@ -18,7 +17,7 @@ class UpdateTreatmentDetailRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, ValidationRule|array<mixed>|string>
+     * // @return array<string, ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
