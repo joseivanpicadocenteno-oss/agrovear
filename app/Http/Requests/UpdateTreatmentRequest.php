046419c94bff1bb0c15a55b69c3803b6034aaa8f
JoseIvanPicado <Jose.Picado8@tecnacional.edu.ni>
2026-07-29 10:21:50 -0600

    Fix(TreatmentRequest): Agregando reglas y autorizacion de UpdateRequest

--- app/Http/Requests/UpdateTreatmentRequest.php
+++ app/Http/Requests/UpdateTreatmentRequest.php
@@ -2,28 +2,24 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateTreatmentRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'start_date' => 'sometimes|date',
+            'end_date' => 'sometimes|date',
+            'diagnosis' => 'sometimes|string|max:500',
+            'observations' => 'sometimes|nullable|string|max:255',
+            'active' => 'sometimes|boolean',
+            'animal_id' => 'sometimes|exists:animals,id',
         ];
     }
 }
