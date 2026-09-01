7626b0a0cb3a6b94f7e8145c94e632f9e35fe728
JoseIvanPicado <Jose.Picado8@tecnacional.edu.ni>
2026-09-01 00:36:21 -0600

    Fix(FarmRequest): actualizacion de metodo Update

--- app/Http/Requests/UpdateFarmRequest.php
+++ app/Http/Requests/UpdateFarmRequest.php
@@ -6,29 +6,28 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateFarmRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * //@return array<string, ValidationRule|array<mixed>|string>
-     */
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'active' => $this->has('active') ? 1 : 0,
+        ]);
+    }
+
     public function rules(): array
     {
-    return [
-        'name' => 'required|string|max:255',
-        'department' => 'required|string|max:255',
-        'municipality' => 'required|string|max:255',
-        'address' => 'nullable|string|max:255',
-        'phone' => 'required|string|min:8|max:20',
-        'description' => 'nullable|string|max:400',
-        'active' => 'boolean',
-    ];
+        return [
+            'name' => 'required|string|max:255',
+            'department' => 'required|string|max:255',
+            'municipality' => 'required|string|max:255',
+            'address' => 'nullable|string|max:255',
+            'phone' => 'required|string|min:8|max:20',
+            'description' => 'nullable|string|max:400',
+            'active' => 'boolean',
+        ];
     }
 }
